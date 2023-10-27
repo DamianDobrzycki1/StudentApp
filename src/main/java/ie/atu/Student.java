baf5bb7d83dd216058c177ab9f7c8f736764ff09
DamianDobrzycki1 <118450532+DamianDobrzycki1@users.noreply.github.com>
2023-10-27 21:06:25 +0100

    Added more "this." statements and new public classes + Constructors + Setter + Getters

--- src/main/java/ie/atu/Student.java
+++ src/main/java/ie/atu/Student.java
@@ -5,17 +5,43 @@ public class Student {
     String studentName;
     String studentEmail;
     String studentCourse;
+    String studentAName;
+    String studentAEmail;
+    String studentACourse;
+    String studentBName;
+    String studentBEmail;
+    String studentBCourse;
+
+    
     public Student(){
         this.studentName = " ";
         this.studentEmail = " ";
         this.studentCourse = " ";
+        this.studentAName = " ";
+        this.studentAEmail = " ";
+        this.studentACourse = " ";
+        this.studenBtName = " ";
+        this.studentBEmail = " ";
+        this.studentBCourse = " ";
     }
 
+    //Constructor
     public Student(String studentName, String studentEmail, String studentCourse) {
         this.studentName = studentName;
         this.studentEmail = studentEmail;
         this.studentCourse = studentCourse;
     }
+    public Student(String studentAName, String studentAEmail, String studentACourse){
+        this.studentAName = studentAName;
+        this.studentAEmail = studentAEmail;
+        this.studentACourse = studentACourse;
+    }
+    
+    public Student(String studentAName, String studentAEmail, String studentACourse){
+        this.studentAName = studentAName;
+        this.studentAEmail = studentAEmail;
+        this.studentACourse = studentACourse;
+        
     public void getUserInput(){
         Scanner scanner = new Scanner(System.in);
 
@@ -29,9 +55,38 @@ public class Student {
         this.studentCourse = scanner.nextLine();
     }
 
+    // Default Constructor
     public void displayInfo() {
         System.out.println(" " + studentName);
         System.out.println(" " + studentEmail);
         System.out.println(" " + studentCourse);
     }
+
+    //Getters
+
+    public String getStudentName(){
+        return studentName;
+    }
+    public String getStudentEmail(){
+        return studentEmail;
+    }
+    public String getStudentCourse(){
+        return studentCourse;
+    }
+
+    //Setters
+    public void setStudentName(String studentName){
+        this.studentName = studentName;
+    }
+    public void setStudentEmail(String studentEmail){
+        this.studentEmail = studentEmail;
+    }
+    public void setStudentCourse(String studentCourse){
+        this.studentCourse = studentCourse;
+    }
+
+    
+
+
+    
 }
